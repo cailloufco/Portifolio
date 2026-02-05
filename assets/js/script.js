a32bc9018389b35d926f39293cbc5fd188775903
Caio <caioextreme000@gmail.com>
2026-02-05 16:39:06 -0300

    tema escuro/claro

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const botao = document.getElementById("ligar");
+const iconTema = document.getElementById("iconCorTema");
+
+let temaClaro = false;
+
+botao.addEventListener("click", function () {
+  console.log("Botão clicado!");
+
+  temaClaro = !temaClaro;
+  iconTema.src = temaClaro ? "assets/imgs/sun-solid-full.svg" : "assets/imgs/moon-solid-full.svg";
+
+
+  document.body.classList.toggle("light");
+  document.querySelector(".container").classList.toggle("light");
+});
